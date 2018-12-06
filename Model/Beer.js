cad778af412b6288704f707cee66bf4f010429f8
Rafaela Santos <rafonha@hotmail.com>
2018-12-05 23:04:04 -0200

    modify class Beer

--- Model/Beer.js
+++ Model/Beer.js
@@ -1,7 +1,17 @@
 class Beer {
-    constructor() {
-        maxTemperature = 0;
-        minTemperature = 0;
+    constructor(max, min) {
+        this._maxTemperature = max;
+        this._minTemperature = min;
+
+        Object.freeze(this);
+    }
+
+    get maxTemp() {
+        return this._maxTemperature;
+    }
+
+    get minTemp() {
+        return this._minTemperature;
     }
 
 }
